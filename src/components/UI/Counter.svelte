df600863b0ce8253f129242e97b0570b93f3d642
Henrique Buss <henrique.buss@hotmail.com>
2020-11-05 15:29:48 -0300

    Added German, counter can go down

--- src/components/UI/Counter.svelte
+++ src/components/UI/Counter.svelte
@@ -3,6 +3,8 @@
   let counter = 0;
 </script>
 
-<button on:click={() => (counter += 1)}>
+<button on:click={() => (counter += 1)}> + </button>
+<button on:click={() => (counter = 0)}>
   {$_('main.numbers.counter_format', { values: { n: counter } })}
 </button>
+<button on:click={() => (counter > 0 ? (counter -= 1) : counter)}>-</button>
--- src/config/l10n.js
+++ src/config/l10n.js
@@ -4,8 +4,9 @@ const locales = {
     "pt-pt": "Português (PT)",
     "en-us": "English (US)",
     "en-gb": "English (GB)",
-    ar: "عربي",
-    ja: "日本人",
+    "de-de": "Deutsche (DE)",
+    "ar-sa": "عربي (SA)",
+    "ja": "日本人",
 };
 
 // Locale to show when we dont' support the requested locale
